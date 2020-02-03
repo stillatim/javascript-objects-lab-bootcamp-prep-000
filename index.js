9f5ccdf1712c2e8001bb1dcb7355829b39bf2646
Justin Evans <evansjustindavid@gmail.com>
2020-02-03 20:28:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {cake: 'eggs'}
 
 function updateObjectWithKeyAndValue(object, a, b) {
-  return object.assign({}, object, {()})
+  return object.assign({}, object, {[a] = b}})
   
 }
 
