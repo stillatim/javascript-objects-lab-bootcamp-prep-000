b83979111116d46a53d4f395e9e9cb83d0376532
Justin Evans <evansjustindavid@gmail.com>
2020-02-03 20:27:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var recipes = {cake: 'eggs'}
 
 function updateObjectWithKeyAndValue(object, a, b) {
-  object[a] = b; 
-  return object
+  
+  
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, a, b) {
